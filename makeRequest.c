caae6bf8904720e67e13a2aefa7afbba39be7545
MarySnik <mary.snikk@gmail.com>
2025-08-13 16:23:05 +0800

    new changes

--- makeRequest.c
+++ makeRequest.c
@@ -7,7 +7,6 @@
 #include <openssl/evp.h>
 
 #include "apikeys.h"
-
 // get word
 // setup curl
 // setup link
@@ -17,161 +16,174 @@ struct data {
     size_t size;
 };
 
-size_t handleMemory(char *ptr, size_t size, size_t nmemb, void *userdata) {
+
+// receive data from the url->returns the number of bytes successfully handled
+// buffer -> chunk of data that just received
+// size -> size of each data element in bytes
+// number of members
+// user-provier data, our struct
+
+char *buildTime(){
+    time_t curTime = time(0);
+    char *curTime_str = malloc(20);
+    if(!curTime_str) return 5;
+    snprintf(curTime_str, 20, "%lld", (long long)curTime);
+    return curTime_str;
+}
+
+
+size_t write_data(void *buffer, size_t size, size_t nmemb, void *userInput){
+    // total number of bytes in this chunk
+    // the amount you'll append to your sotrage buffer
     size_t realSize = size * nmemb;
-    struct data *mem = (struct data *)userdata;
+    struct data *tempData = (struct data *)userInput;
 
-    char *temp = realloc(mem->response, mem->size + realSize + 1);
-    if (!temp) {
-        fprintf(stderr, "Out of memory\n");
-        return 0;
-    }
-    mem->response = temp;
+    // reallocs our currently struct for the response
+    char *ptr = realloc(tempData->response, temp->size + realSize + 1);
+    if(!ptr) return 5; //out of memory
 
-    memcpy(&(mem->response[mem->size]), ptr, realSize);
-    mem->size += realSize;
-    mem->response[mem->size] = 0;
+    // new reallocated block
+    temp->response = ptr;
+
+    // a new data to our buffer
+    memcpy(&(temp->response[temp->size]), buffer, realSize);
+    // recalculate the size
+    temp->size += realSize;
+    temp->response[temp->size] = 0;
 
     return realSize;
+};
+
+char *buildUuid(){
+   uuid_t binuuid;
+   uuid_generate(binuuid);
+
+   char *uuid_str = malloc(37);
+   if(!uuid_str) return 5;
+   
+   uuid_unparse(binuuid, uuid_str);
+
+   return uuid_str;
 }
 
-int buildUUID(char *salt) {
-    uuid_t binUuid;
-    uuid_generate_random(binUuid);
-    uuid_unparse_lower(binUuid, salt);
-    return 0;
-}
+char *buildSha256(*userInput, *salt, *curTime){
 
-int sha256_string(const char *input, char output[65]) {
+    snprintf(&sign, signof(sign), "%s%s%s%s%s", API_ID, userInput, salt, curTime, API_SECRET);
+
+    unsigned char hash[SHA256_DIGEST_LENGTH];
+    unsigned int hash_len;
+
+    // 1. create a message digest context
     EVP_MD_CTX *mdctx = EVP_MD_CTX_new();
-    if (!mdctx) return 1;
-
-    if (EVP_DigestInit_ex(mdctx, EVP_sha256(), NULL) != 1) {
-        EVP_MD_CTX_free(mdctx);
-        return 2;
+    if(mdctx == NULL){
+        perror("EVP_MD_CTX_new");
+        return 6;
     }
 
-    if (EVP_DigestUpdate(mdctx, input, strlen(input)) != 1) {
-        EVP_MD_CTX_free(mdctx);
-        return 3;
+    // 2. initialize the digest context for sha-256;
+    if(EVP_DigestInit_ex(mdctx, EVP_sHA256(), NULL) != 1){
+        perrror("EVP_DigestInit_ex");
+        return 1;
+    }
+    
+    // 3. hash the input
+    if(EVP_DigestUpdate(mdctx, userInput, strlen(userInput)) != 1){
+        perror("EVP_DigestUpdate");
+        return 6;
     }
 
-    unsigned char hash[EVP_MAX_MD_SIZE];
-    unsigned int lengthOfHash = 0;
-
-    if (EVP_DigestFinal_ex(mdctx, hash, &lengthOfHash) != 1) {
-        EVP_MD_CTX_free(mdctx);
-        return 4;
+    // 4. finalize the hash
+    if(EVP_DigestFinal_ex(mdctx, hash, &hash_len) != 1){
+        perror("EVP_DigestFinal_ex");
+        return 7;
     }
 
+    // 5. free the context;
     EVP_MD_CTX_free(mdctx);
 
-    for (unsigned int i = 0; i < lengthOfHash; i++) {
-        sprintf(output + (i * 2), "%02x", hash[i]);
+    // 6. convert to hex string
+    char sign[hash_len * 2 + 1];
+    for(unsigned int i = 0; i < hash_len; i++){
+        sprintf(sign + (i * 2), "%02x", hash[i]);
     }
-    output[64] = 0;
+
+    sign[hash_len * 2] = '\0';
+
+    printf("this is sha256: %s\n", sign);
+
+    return sign;
+}
+
+
+int createUrl(char *url, char *userInput, char *option){
+
+    if(strlen(userInput) > 20) return 1;
+
+    // for now: 1 word; <20 symbols
+    
+    // LINK FORMAT:
+    // url?q=<word>&langType=auto&appKey=<myAppKey>&dicts=<option>&salt=<salt>&sign=<mySign>&signType=v3&curtime=<timestamp>&docType=json>
+    char startUrl[] = "https://openapi.youdao.com/v2/dict";
+    char *salt = buildUuid();
+    char *curTime = buildTime();;
+    char *sign = buildSha256(userInput, salt, curTime);
+
+    snprintf(url, lengthTotal, 
+        "%s?q=%s&langType=auto&appKey=%s&dicts=%s&salt=%s&sign=%s&signType=v3&curtime%s&docType=json", 
+        startUrl, userInput, API_KEY, option, salt, sign, curTime);
+
+    printf("full url string: %s\n");
+
+
     return 0;
 }
 
-int buildSign(char *sign_string, const char *userInput, const char *salt, const char *curtime) {
-    char buffer[1024];
-    snprintf(buffer, sizeof(buffer), "%s%s%s%s%s", API_KEY, userInput, salt, curtime, API_SECRET);
-    return sha256_string(buffer, sign_string);
-}
-
-int buildPostFields(char *postfields, size_t size, const char *encodedInput, const char *option, const char *salt, const char *originalInput) {
-    time_t curtime = time(NULL);
-    char curtime_str[20];
-    snprintf(curtime_str, sizeof(curtime_str), "%ld", (long)curtime);
-
-    char sign_string[65];
-    if (buildSign(sign_string, originalInput, salt, curtime_str) != 0) {
-        fprintf(stderr, "Failed to build sign\n");
-        return 1;
-    }
-
-    printf("API_KEY: %s\n", API_KEY);
-printf("API_SECRET: %s\n", API_SECRET);
-printf("User Input: %s\n", originalInput);
-printf("Salt: %s\n", salt);
-printf("Curtime: %s\n", curtime_str);
-printf("Sign string: %s\n", sign_string);
-printf("Postfields: %s\n", postfields);
 
 
-    snprintf(postfields, size,
-        "q=%s&langType=auto&appKey=%s&dicts=%s&salt=%s&sign=%s&curtime=%s&docType=json",
-        encodedInput, API_KEY, option, salt, sign_string, curtime_str);
+// ce or ec
+int makeRequest(char *option){
 
-    return 0;
-}
+    char *url = malloc(512);
+    if(!url) return 5;
 
-int makeRequest(char *option) {
-    struct data response_data = { NULL, 0 };
+    char *userInput = malloc(99);
+    if(!userInput) return 1;
 
-    CURL *curl = NULL;
+    
+    printf("Input your word:\n>> ");
+    if((scanf("%99s", userInput)) != 1){
+        return 5;
+    };
+    
+
+    struct data myData = {0};
+    // 2.setup curl
+    
+    if(curl_global_init(CURL_GLOBAL_DEFAULT) != CURLE_OK) return 5;
+
     CURLcode res;
+    CURL *curl = curl_easy_init();
+    if(curl){
 
-    char input[100];
-    printf("Input your word:\n");
-    if (scanf("%99s", input) != 1) {
-        fprintf(stderr, "Failed to read input\n");
-        return 1;
-    }
-
-    curl_global_init(CURL_GLOBAL_ALL);
-    curl = curl_easy_init();
-    if (!curl) {
-        fprintf(stderr, "Failed to init curl\n");
-        curl_global_cleanup();
-        return 1;
-    }
-
-    char *encoded_input = curl_easy_escape(curl, input, 0);
-    if (!encoded_input) {
-        fprintf(stderr, "Failed to URL encode input\n");
+        //3. write link
+        createUrl(url, userInput, option);
+        
+        curl_easy_setopt(curl, CURLOPT_URL, "link");
+        // send data to this function
+        curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_data);
+        // pass our chunk struct ti o here
+        curl_easy_setopt(curl, CURLOPT_WRITEDATA, &myData); // get data
+        
+        res = curl_easy_perform(curl);
+        
+        free(data.response);
         curl_easy_cleanup(curl);
-        curl_global_cleanup();
-        return 1;
-    }
+        curl_global_cleanup();;
 
-    char salt[37];
-    buildUUID(salt);
-
-    char postfields[512];
-    if (buildPostFields(postfields, sizeof(postfields), encoded_input, option, salt, input) != 0) {
-        curl_free(encoded_input);
-        curl_easy_cleanup(curl);
-        curl_global_cleanup();
-        return 1;
-    }
-
-    printf("POSTFIELDS: %s\n", postfields);
-
-    curl_easy_setopt(curl, CURLOPT_URL, "https://openapi.youdao.com/v2/dict");
-    curl_easy_setopt(curl, CURLOPT_POST, 1L);
-    curl_easy_setopt(curl, CURLOPT_POSTFIELDS, postfields);
-    curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, handleMemory);
-    curl_easy_setopt(curl, CURLOPT_WRITEDATA, &response_data);
-
-    res = curl_easy_perform(curl);
-
-    long http_code = 0;
-    curl_easy_getinfo(curl, CURLINFO_RESPONSE_CODE, &http_code);
-    printf("HTTP response code: %ld\n", http_code);
-
-    if (res != CURLE_OK) {
-        fprintf(stderr, "Curl error: %s\n", curl_easy_strerror(res));
-    } else if (response_data.response == NULL || response_data.size == 0) {
-        fprintf(stderr, "No response data received.\n");
     } else {
-        printf("Response: %s\n", response_data.response);
+        return 5;
     }
+        
 
-    curl_free(encoded_input);
-    free(response_data.response);
-    curl_easy_cleanup(curl);
-    curl_global_cleanup();
 
-    return 0;
 }
--- makeRequest.h
+++ makeRequest.h
@@ -11,12 +11,13 @@ struct data {
   size_t size;
 };
 
-int makeRequest(char *option);
-int handleResponse(char *response_data);
-int sha256_string(const char *input, char outputBuffer[65]);
-int buildUUID(char *salt);
-int buildSign(char *sign_string, char *userInput, char *uuid, char *curTime);
-int creatingLink(char *url, char *userInput, char *option);
-size_t handleMemory(char *ptrToData, size_t size, size_t numMembers, void *userInput);
+// int creatingUrl(char *url, char *userInput, char *option);
+// int makeRequest(char *option);
+// int handleResponse(char *response_data);
+// int sha256_string(const char *input, char outputBuffer[65]);
+// int buildUUID(char *salt);
+// int buildSign(char *sign_string, char *userInput, char *uuid, char *curTime);
 
+// size_t handleMemory(char *ptrToData, size_t size, size_t numMembers, void *userInput);
 #endif 
+
